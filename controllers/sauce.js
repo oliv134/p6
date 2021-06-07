ac0c7fc05faf9bcf8d0cefbf344c75a93cf9183d
oliv134 <oliv134@oliv134.net>
2021-06-07 09:38:22 +0200

    first commit

---
+++ controllers/sauce.js
@@ -0,0 +1,60 @@
+const { json } = require("express");
+const Sauce = require("../models/sauce");
+const fs = require("fs");
+
+exports.createThing = (req, res, next) => {
+  console.log(req.body.sauce);
+  const sauceObject = JSON.parse(req.body.sauce);
+  delete sauceObject._id;
+  const sauce = new Sauce({
+    ...sauceObject,
+    imageUrl: `${req.protocol}://${req.get("host")}/images/${
+      req.file.filename
+    }`,
+  });
+  sauce
+    .save()
+    .then(() => res.status(201).json({ message: "Objet enregistré !" }))
+    .catch((error) => res.status(400).json({ error }));
+};
+
+exports.getThings = (req, res, next) => {
+  Thing.find()
+    .then((sauces) => res.status(200).json(sauces))
+    .catch((error) => res.status(400).json({ error }));
+};
+
+exports.modifyThing = (req, res, next) => {
+  const sauceObject = req.file
+    ? {
+        ...JSON.parse(req.body.sauce),
+        imageUrl: `${req.protocol}://${req.get("host")}/images/${
+          req.file.filename
+        }`,
+      }
+    : { ...req.body };
+  Thing.updateOne(
+    { _id: req.params.id },
+    { ...sauceObject, _id: req.params.id }
+  )
+    .then(() => res.status(200).json({ message: "Objet modifié !" }))
+    .catch((error) => res.status(400).json({ error }));
+};
+exports.getThing = (req, res, next) => {
+  Thing.findOne({ _id: req.params.id })
+    .then((sauce) => res.status(200).json(sauce))
+    .catch((error) => res.status(404).json({ error }));
+};
+
+exports.deleteThing = (req, res, next) => {
+  Thing.findOne({ _id: req.params.id })
+    .then((sauce) => {
+      const filename = sauce.imageUrl.split("/images/")[1];
+      fs.unlink(`images/${filename}`, () => {
+        Thing.deleteOne({ _id: req.params.id })
+          .then(() => res.status(200).json({ message: "Objet supprimé !" }))
+          .catch((error) => res.status(400).json({ error }));
+      });
+    })
+    .catch((error) => res.status(500).json({ error }));
+};
---
+++ routes/sauce.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+
+const auth = require ('../middleware/auth')
+const multer = require ('../middleware/multer-config')
+
+const sauceCtrl = require('../controllers/sauce')
+
+router.get('/', auth, sauceCtrl.getThings);
+router.get('/:id', auth, sauceCtrl.getThing);
+router.post('/', auth, multer, sauceCtrl.createThing);
+router.put('/:id', auth, multer,  sauceCtrl.modifyThing);
+router.delete('/:id', auth, sauceCtrl.deleteThing);
+router.post('/:id/like', auth, sauceCtrl.getThing);
+module.exports = router;
